f1862c0f661537f76544583bb6a30d3015042f39
Michael Jakobsen <michael@rpair.co.uk>
2022-03-29 19:12:03 +0100

    created models using mongoose schemas for company, user and repairs

---
+++ server/models/company.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const ratingSchema = new mongoose.Schema({
+  text: String,
+  rating: Number,
+  user: { type: mongoose.Schema.ObjectId, ref: "User", required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const companySchema = new mongoose.Schema({
+  companyName: { type: String, required: true, unique: true },
+  description: { type: String, required: false },
+  email: { type: String, required: true, unique: true },
+  number: { type: Number, required: false, unique: true },
+  postcode: { type: String, required: true },
+  address: { type: String, required: true },
+  mobileRepair: { type: Boolean, required: false },
+  computerRepair: { type: Boolean, required: false },
+  electronicRepair: { type: Boolean, required: false },
+  watchRepair: { type: Boolean, required: false },
+  jewelleryRepair: { type: Boolean, required: false },
+  // * Embedded Schema for Ratings
+  ratings: [ratingSchema],
+});
+
+export default mongoose.model("Company", companySchema);
