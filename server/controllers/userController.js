33bd28cf9a65b5e2616008ee792b18207228adff
Michael Jakobsen <michael@rpair.co.uk>
2022-03-29 19:39:24 +0100

    added two new user controllers for login and index of users

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,9 +7,10 @@ async function register(req, res, next) {
   try {
     const userAlreadyExists = await User.findOne({ eMail: req.body.eMail });
     if (userAlreadyExists) {
-      return res
-        .status(400)
-        .json({ message: "💔 The user already exists, please try again 🙏" });
+      return res.status(400).json({
+        message:
+          "💔 Oh no! The user already exists, please try another email 🙏",
+      });
     }
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
@@ -24,6 +25,55 @@ async function register(req, res, next) {
   }
 }
 
+// ! LOGIN CONTROLLER FOR USERS
+async function login(req, res, next) {
+  try {
+    const user = await User.findOne({ userName: req.body.eMail });
+    if (!user) {
+      // ! if the user does not exists
+      return res.status(400).json({
+        message:
+          "🚷 No user with that email exists! Please check the email and try again.",
+      });
+    }
+
+    const passwordsMatch = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!passwordsMatch) {
+      return res.status(400).json({
+        // ! IF PASSWORD IS NOT A MATCH
+        message: "🔐 You have used an incorrect password, please try again!",
+      });
+    }
+
+    const payload = {
+      eMail: user.eMail,
+      role: user.role,
+    };
+
+    console.log(process.env.JWT_SECRET);
+    const token = jwt.sign(payload, process.env.JWT_SECRET);
+    console.log(token);
+    res.status(200).json({ message: "Login Successful ✅", token });
+    console.log("User logged in successfully");
+  } catch (e) {
+    next(e);
+  }
+}
+
+async function index(req, res) {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (e) {
+    res.send({ message: "There was a problem finding the users!" });
+  }
+}
+
 export default {
   register,
+  login,
+  index,
 };
